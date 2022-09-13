d0701dc9f982bc5738a07669903b8806cba264d8
Weijie Zhao <maggch@outlook.com>
2022-09-13 19:19:02 +0800

    update

---
+++ .vitepress/components/WebQa.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+
+<div>
+<br/>
+<br/>
+<p>请勾选确认事项后点击查看帮助</p>
+
+<label> <input id="helpItem" type="checkbox" />我需要一起看功能相关的帮助</label>
+
+<br/>
+<button @click="helpClick">查看帮助</button>
+</div>
+
+</template>
+<script setup>
+
+function helpClick(){
+    if(document.querySelector("#helpItem").checked){
+        window.location = "/guide/qa.html";
+    }else{
+        alert("请先勾选确认事项")
+    }
+}
+
+(function(){
+    // setTimeout(()=>{
+    //     document.documentElement.querySelector('.VPFooter').remove();
+    // },10)
+    setInterval(()=>{
+        try{ 
+            document.documentElement.querySelector('.VPNav').style.display='none';
+            document.documentElement.querySelector('.VPFooter').style.display='none';
+        }catch{}
+    },100)
+})();
+</script>
