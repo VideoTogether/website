f7e71a0ff780f3f0355e8b877505e9df8229e5d4
Weijie Zhao <maggch@outlook.com>
2022-12-06 18:38:26 +0800

    fix website qa

---
+++ .vitepress/components/LanguageWebsiteQA.vue
@@ -0,0 +1,12 @@
+<script>
+(function(){
+    if(typeof window != 'undefined'){
+        if(navigator.language.toLowerCase().startsWith('zh')){
+            window.location = '/zh-cn/guide/website_qa.html'
+        }else{
+            window.location = '/en-us/guide/qa.html'
+        }
+    }
+
+})()
+</script>
---
+++ guide/website_qa.md
@@ -0,0 +1,11 @@
+# Q&A
+
+Please select the page language first
+
+请先选择语言
+
+
+<script setup>
+import Language from '../.vitepress/components/LanguageWebsiteQA.vue'
+</script>
+<Language />
